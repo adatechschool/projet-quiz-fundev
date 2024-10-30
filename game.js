47c0c361af0d9dc4f432abf687e86b3321ada158
Marie Marchal <you@example.com>
2024-10-30 11:52:53 +0100

    étape5

--- game.js
+++ game.js
@@ -1,19 +1,44 @@
 import { quizzInsolite } from './question.js';
 
-let firstQuestion = document.querySelector(".question");
-firstQuestion.innerHTML = quizzInsolite.questions[0].text;
+let questionContainer = document.querySelector(".question");
 
 let optionsContainer = document.querySelector(".options");
 optionsContainer.innerHTML = ""; 
-quizzInsolite.questions[0].options.forEach(element => {
-    const button = document.createElement('button'); 
-    button.innerText = element; 
-    button.classList.add('option'); 
-    optionsContainer.appendChild(button); 
+
+let buttonNext = document.querySelector("#next-button");
+buttonNext.innerText = "suivant";
+
+let currentQuestionIndex = 0;
+
+
+
+function loadQuestion() {
+    optionsContainer.innerHTML = '';
+    let question = quizzInsolite.questions[currentQuestionIndex];
+    questionContainer.innerText = question.text;
+
+    question.options.forEach(element => {
+        const button = document.createElement('button');
+        button.innerText = element;
+        button.classList.add('option');
+        optionsContainer.appendChild(button);
+      });
+}
+
+  buttonNext.addEventListener('click', () => {
+    currentQuestionIndex++;
+    if ( currentQuestionIndex < quizzInsolite.questions.length) {
+        loadQuestion();
+      } else {
+        questionContainer.innerText ="Fin du Quizz";
+        optionsContainer.innerHTML = "";
+        buttonNext.style.display = "none";
+      }
 });
 
-let button = document.querySelector("#next-button");
-button.innerText = "suivant";
+loadQuestion();
+
+
 
 
 
