0afb0aa389d9504ec6b64ded2d8c6c70ba45946c
Marie Marchal <you@example.com>
2024-10-31 11:44:04 +0100

    PUTAINdEtape7

--- game.js
+++ game.js
@@ -2,32 +2,52 @@ import { quizzInsolite } from './question.js';
 
 let questionContainer = document.querySelector(".question");
 
-let optionsContainer = document.querySelector(".options");
-optionsContainer.innerHTML = ""; 
+let optionsContainer = document.querySelector(".options"); 
 
 let buttonNext = document.querySelector("#next-button");
 buttonNext.innerText = "suivant";
 
 let replayButton = document.querySelector('#replay-button');
 
+let indexQuestion = 0;
+
+let answers = quizzInsolite.questions[indexQuestion].correct_answer;
+
 let currentQuestionIndex = 0;
+
+let bloquage = true;
+
+
+
+
 
 
 function loadQuestion() {
     optionsContainer.innerHTML = '';
+    buttonNext.disabled = true;
+
     let question = quizzInsolite.questions[currentQuestionIndex];
     questionContainer.innerText = question.text;
 
-    question.options.forEach(element => {
+    question.options.forEach(contenu => {
         const button = document.createElement('button');
-        button.innerText = element;
+        button.innerText = contenu;
         button.classList.add('option');
         optionsContainer.appendChild(button);
+
+        button.addEventListener("click", () => {
+            if (bloquage) {
+            bloquage = false;
+            buttonNext.disabled = false;
+            checkAnswer(button, question.correct_answer);}
+});
+
       });
 }
-
+console.log(bloquage)
   
-  buttonNext.addEventListener('click', () => {
+buttonNext.addEventListener('click', () => {
+    if (!bloquage) {
     currentQuestionIndex++;
     if ( currentQuestionIndex < quizzInsolite.questions.length) {
         loadQuestion();
@@ -37,7 +57,8 @@ function loadQuestion() {
         buttonNext.style.display = "none";
         replayButton.style.display = "block";
       }
-  });
+        bloquage = true;
+  }});
   
   
   replayButton.addEventListener('click', () => {
@@ -45,7 +66,19 @@ function loadQuestion() {
     replayButton.style.display = "none";
     buttonNext.style.display = "block";  
     loadQuestion();
-  });
+    bloquage = true;
+});
 
+
+
+function checkAnswer(boutonSelectionne, answers) {
+    bloquage = false;
+    buttonNext.disabled = false;
+    if (boutonSelectionne.innerText === answers) {
+        boutonSelectionne.style.backgroundColor = "green";
+    } else {
+        boutonSelectionne.style.backgroundColor = "red";
+    }
+}
 
 loadQuestion();
