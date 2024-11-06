8aba3545fee3e6ef50a5588588799f87083c907e
Jude Abraham <jude.abraham@epitech.eu>
2024-11-06 10:49:55 +0100

    [ADD] timer and progress bar reset after replay and documentation

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ let affichageScore = document.querySelector("#afficheScore");
 
 let currentQuestionIndex = 0;
 
-let bloquage = true;
+let bloquage = true;    //variable pour bloquer le fait de cliquer sur le bouton next ou sur les options
 
 let score = 0;
 
@@ -22,22 +22,22 @@ affichageScore.innerHTML = `Ton score est de ${score} points !`
 buttonNext.style.display = "block";
 
 let barProgress = 0;
-document.querySelector('#progress').setAttribute('value', barProgress);
+document.querySelector('#progress').setAttribute('value', barProgress); //injecter une valeur a la bar de progrès
 
 let timer = 30;
 
-document.querySelector('#timer').innerHTML = timer;
+document.querySelector('#timer').innerHTML = timer; //afficher le timer dans la page html
 
 let setTimer = setInterval(() => {
     if (timer > 0)
         timer--;
-    document.querySelector('#timer').innerHTML = timer;
-}, 1000);
+    document.querySelector('#timer').innerHTML = timer;                     //fonction qui va décrémenter la valeur du timer de 1 chaque seconde
+}, 1000);                                                           //on met 1000 car c'est en millisecondes
 
 
 let timeout = setTimeout(() => {
     bloquage = false;
-    buttonNext.disabled = false;
+    buttonNext.disabled = false;            //fonction créer un timer de 30 secondes, si on ne repond pas avant la fin il bloque les réponses
 }, 30000);
 
 
@@ -49,14 +49,14 @@ function loadQuestion() {
     optionsContainer.innerHTML = '';
     buttonNext.disabled = true;
 
-    question.options.forEach(contenu => {
+    question.options.forEach(contenu => {                   //fonction qui va créer chaque bouton et lui attribuer une valeur
         const button = document.createElement('button');
         button.innerText = contenu;
         button.classList.add('option');
         optionsContainer.appendChild(button);
 
-        button.addEventListener("click", () => {
-            if (bloquage) {
+        button.addEventListener("click", () => {            //fonction qui va, si on choisis une option, bloquer les autres options et checkAnswer
+            if (bloquage) {         //check si on pas a deja appuye sur une autre option dans la meme question
                 bloquage = false;
                 buttonNext.disabled = false;
                 checkAnswer(button, question.correct_answer);
@@ -65,7 +65,7 @@ function loadQuestion() {
     });
 }
 
-let manageTime = () =>
+let manageTime = () =>                          //fonction qui va reset les 2 timers avec leurs valeurs et les relancer
 {
     clearTimeout(timeout);
     clearInterval(setTimer);
@@ -82,13 +82,13 @@ let manageTime = () =>
     }, 1000);
 }
 
-buttonNext.addEventListener('click', () => {
-    if (!bloquage) {
+buttonNext.addEventListener('click', () => {                    //fonction qui va detecter le click sur le bouton suivant et passer a la prochaine question
+    if (!bloquage) {        //check si on a bien deja repondu a la question
         currentQuestionIndex++;
         affichageScore.innerHTML = `Ton score est de ${score} points !`
         if (currentQuestionIndex < quizzInsolite.questions.length)
             loadQuestion();
-        else {
+        else {                                                  //check si on est bien arrivé a la fin du quizz
             questionContainer.innerText =` Fin du Quizz ! Tu as ${score} points !`;
             optionsContainer.innerHTML = "";
             buttonNext.style.display = "none";
@@ -96,16 +96,20 @@ buttonNext.addEventListener('click', () => {
             document.querySelector('#timer').style.display = "none";
         }
         bloquage = true;
-        barProgress = (100 / 7) * currentQuestionIndex;
+        barProgress = (100 / 7) * currentQuestionIndex;                             //incrémentation de la bar de progression
         document.querySelector('#progress').setAttribute('value', barProgress);
         manageTime();
     }
 });
 
-replayButton.addEventListener('click', () => {
+replayButton.addEventListener('click', () => {          //fonction qui va tout reset et nous renvoyer au debut du quizz
     currentQuestionIndex = 0;
     replayButton.style.display = "none";
     buttonNext.style.display = "block";
+    barProgress = (100 / 7) * currentQuestionIndex;
+    document.querySelector('#progress').setAttribute('value', barProgress);
+    manageTime();
+    document.querySelector('#timer').style.display = "block";
     loadQuestion();
     bloquage = true;
     score = 0;
@@ -113,14 +117,14 @@ replayButton.addEventListener('click', () => {
 });
 
 
-function checkAnswer(boutonSelectionne, answers) {
+function checkAnswer(boutonSelectionne, answers) {         //fonction qui va check si la reponse est bonne ou pas
     bloquage = false;
     buttonNext.disabled = false;
     if (boutonSelectionne.innerText === answers) {
-        boutonSelectionne.classList.add('right');
+        boutonSelectionne.classList.add('right');       //la reponse devient verte car la classe right a comme attribut cette couleur (bonne réponse)
         score++;
     } else
-        boutonSelectionne.classList.add("wrong");
+        boutonSelectionne.classList.add("wrong");       //la reponse devient rouge car la classe wrong a comme attribut cette couleur (fausse réponse)
 }
 
-loadQuestion();
+loadQuestion(); //fonction principale qui regroupe toutes les fonctions
