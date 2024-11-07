88afa61e043a93ea407b9db00f0c84822e875026
Jude Abraham <jude.abraham@epitech.eu>
2024-11-07 10:14:04 +0100

    [CHANGE] make the code more reusable by creating more variables

--- game.js
+++ game.js
@@ -10,6 +10,7 @@ let affichageScore = document.querySelector("#afficheScore");
 let currentQuestionIndex = 0;
 let bloquage = true;    //variable pour bloquer le fait de cliquer sur le bouton next ou sur les options
 let score = 0;
+let maxScore = 20;
 affichageScore.innerHTML = `Ton score est de ${score} points !`
 buttonNext.style.display = "block";
 let barProgress = 0;
@@ -21,18 +22,19 @@ document.querySelector('#timer').innerHTML = timer; //afficher le timer dans la 
 
 // LES FONCTIONS
 
+//fonction qui va décrémenter la valeur du timer de 1 chaque seconde
+
 let setTimer = setInterval(() => {
-    if (timer > 0)
-        timer--;
-    document.querySelector('#timer').innerHTML = timer;                     //fonction qui va décrémenter la valeur du timer de 1 chaque seconde
+    timer > 0 ? timer-- : 0;                                        //operation ternaire, if else
+    document.querySelector('#timer').innerHTML = timer;
 }, 1000);                                                           //on met 1000 car c'est en millisecondes
 
-
+//fonction créer un timer de 30 secondes, si on ne repond pas avant la fin il bloque les réponses
 
 let timeout = setTimeout(() => {
     bloquage = false;
-    buttonNext.disabled = false;            //fonction créer un timer de 30 secondes, si on ne repond pas avant la fin il bloque les réponses
-}, 30000);
+    buttonNext.disabled = false;
+}, timer * 1000);
 
 
 function loadQuestion() {
@@ -42,14 +44,18 @@ function loadQuestion() {
     optionsContainer.innerHTML = '';
     buttonNext.disabled = true;
 
-    question.options.forEach(contenu => {                   //fonction qui va créer chaque bouton et lui attribuer une valeur
+    //fonction qui va créer chaque bouton et lui attribuer une valeur
+
+    question.options.forEach(contenu => {
         const button = document.createElement('button');
         button.innerText = contenu;
         button.classList.add('option');
         optionsContainer.appendChild(button);
 
-        button.addEventListener("click", () => {            //fonction qui va, si on choisis une option, bloquer les autres options et checkAnswer
-            if (bloquage) {         //check si on pas a deja appuye sur une autre option dans la meme question
+        //fonction qui va, si on choisis une option, bloquer les autres options et checkAnswer
+
+        button.addEventListener("click", () => {
+            if (bloquage) {                                 //check si on pas a deja appuye sur une autre option dans la meme question
                 bloquage = false;
                 buttonNext.disabled = false;
                 checkAnswer(button, question.correct_answer);
@@ -58,9 +64,9 @@ function loadQuestion() {
     });
 }
 
+//fonction qui va reset les 2 timers avec leurs valeurs et les relancer
 
-
-let manageTime = () =>                          //fonction qui va reset les 2 timers avec leurs valeurs et les relancer
+let manageTime = () =>
 {
     clearTimeout(timeout);
     clearInterval(setTimer);
@@ -69,33 +75,34 @@ let manageTime = () =>                          //fonction qui va reset les 2 ti
     timeout = setTimeout(() => {
         bloquage = false;
         buttonNext.disabled = false;
-    }, 30000);
+    }, timer * 1000);
     setTimer = setInterval(() => {
-        if (timer > 0)
-            timer--;
+        timer > 0 ? timer-- : 0;
         document.querySelector('#timer').innerHTML = timer;
     }, 1000);
 }
 
+//fonction qui permet d'afficher un certain gif par rapport au score
 
-
-let gifByScore = () =>              //fonction qui permet d'afficher un certain gif par rapport au score
+let gifByScore = () =>
 {
     if (score === 0)
         document.querySelector('#gif').setAttribute('src', "https://media1.tenor.com/m/iAooOO3wBt0AAAAd/nul-homer.gif");
-    else if (score <= 5)
+    else if (score <= maxScore * 0.25)
         document.querySelector('#gif').setAttribute('src', "https://media1.tenor.com/m/_IT4iiUqkw4AAAAd/homer-simpson-les-simpson.gif");
-    else if (score <= 10)
+    else if (score <= maxScore * 0.5)
         document.querySelector('#gif').setAttribute('src', "https://media1.tenor.com/m/7zg2nZnGU-QAAAAd/the-office-the.gif");
-    else if (score >= 11 && score <= 15)
+    else if (score >= maxScore * 0.5 + 1 && score <= maxScore * 0.75)
         document.querySelector('#gif').setAttribute('src', "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2016/05/Simpsons.gif");
-    else if (score >= 16 && score <= 19)
+    else if (score >= maxScore * 0.75 + 1 && score <= maxScore - 1)
         document.querySelector('#gif').setAttribute('src', "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnllenJ0Z25tZTU3eGFyNGVoZXd3OXl2YXlyd2MwMWphemQ2Njl5cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0amJzVHIAfl7jMDos/giphy.webp");
-    else if (score === 20)
+    else if (score === maxScore)
         document.querySelector('#gif').setAttribute('src', "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExeWhja3d1eXF4NWVmbGcybXV5YXp3eWF4dmRwN2pnNXdtMzd1YWkyYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6MbhZORlagRIyHKw/giphy.webp")
 }
 
-let getConfetti = () =>             //fonction qui affiche des confetti
+//fonction qui affiche des confetti
+
+let getConfetti = () =>
 {
     confetti({
         particleCount: 300,
@@ -110,7 +117,9 @@ let getConfetti = () =>             //fonction qui affiche des confetti
     });
 }
 
-let endQuizz = () =>            //fonction qui va tout réinitialiser pour relancer le quizz dès le départ
+//fonction qui va tout réinitialiser pour relancer le quizz dès le départ
+
+let endQuizz = () =>
 {
     questionContainer.innerText =` Fin du Quizz ! Tu as ${score} points !`;
     optionsContainer.innerHTML = "";
@@ -122,9 +131,9 @@ let endQuizz = () =>            //fonction qui va tout réinitialiser pour relan
     getConfetti();
 }
 
+//fonction qui va detecter le click sur le bouton suivant et passer a la prochaine question
 
-
-buttonNext.addEventListener('click', () => {                    //fonction qui va detecter le click sur le bouton suivant et passer a la prochaine question
+buttonNext.addEventListener('click', () => {
     if (!bloquage) {        //check si on a bien deja repondu a la question
         currentQuestionIndex++;
         affichageScore.innerHTML = `Ton score est de ${score} points !`
@@ -133,19 +142,19 @@ buttonNext.addEventListener('click', () => {                    //fonction qui v
         else                                                         //check si on est bien arrivé a la fin du quizz
             endQuizz();
         bloquage = true;
-        barProgress = (100 / 20) * currentQuestionIndex;                             //incrémentation de la bar de progression
+        barProgress = (100 / maxScore) * currentQuestionIndex;                             //incrémentation de la bar de progression
         document.querySelector('#progress').setAttribute('value', barProgress);
         manageTime();
     }
 });
 
+//fonction qui va tout reset et nous renvoyer au debut du quizz
 
-
-replayButton.addEventListener('click', () => {          //fonction qui va tout reset et nous renvoyer au debut du quizz
+replayButton.addEventListener('click', () => {
     currentQuestionIndex = 0;
     replayButton.style.display = "none";
     buttonNext.style.display = "block";
-    barProgress = (100 / 20) * currentQuestionIndex;
+    barProgress = (100 / maxScore) * currentQuestionIndex;
     document.querySelector('#progress').setAttribute('value', barProgress);
     manageTime();
     document.querySelector('#timer').style.display = "block";
@@ -156,10 +165,9 @@ replayButton.addEventListener('click', () => {          //fonction qui va tout r
     affichageScore.innerHTML = `Ton score est de ${score} points !`
 });
 
+//fonction qui va check si la reponse est bonne ou pas
 
-
-
-function checkAnswer(boutonSelectionne, answers) {         //fonction qui va check si la reponse est bonne ou pas
+function checkAnswer(boutonSelectionne, answers) {
     bloquage = false;
     buttonNext.disabled = false;
     if (boutonSelectionne.innerText === answers) {
